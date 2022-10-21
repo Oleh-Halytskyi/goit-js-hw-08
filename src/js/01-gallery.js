134986344efc6f0fdb46faddf70cf61d580c9ae0
Oleh Halytskyi <oleh.halytskyy@gmail.com>
2022-10-21 07:58:39 +0300

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import galleryItems from "./gallery-items";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+function createGalleryItem(galleryItems) {
+    return galleryItems
+      .map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+        `;
+      })
+      .join('');
+}
+
+gallery.innerHTML = createGalleryItem(galleryItems)
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay:250 });
