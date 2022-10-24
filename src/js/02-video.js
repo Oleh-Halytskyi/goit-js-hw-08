20a71c3a2fe1eacd4a5db49cc0f0c02f9b8dae67
Oleh Halytskyi <oleh.halytskyy@gmail.com>
2022-10-24 21:16:37 +0300

    fix bug task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,16 +2,17 @@ const throttle = require("lodash.throttle");
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);    
 
-const player = new Player(iframe);
+const LOCALSTORAGE_TIME = 'videoplayer-current-time';
 
-function setTime (evt) {
+function setTime(evt) {
     const time = evt.seconds
-    const timeData = localStorage.setItem('videoplayer-current-time', time);
-    
+    const timeData = localStorage.setItem(LOCALSTORAGE_TIME, time);      
 }
 const throttled = throttle(setTime, 1000)
 
 player.on('timeupdate', throttled);
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+if (localStorage.getItem(LOCALSTORAGE_TIME)) {
+    player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_TIME))
+}
